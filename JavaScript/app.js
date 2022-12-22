7862bed0da322412d8a5371408dd9a2f67ba4c52
Ihjas <ihjasdev@gmail.com>
2022-12-22 11:12:05 +0530

    creating string array object in console

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,13 @@
-alert("Hello Ihjas");
+// alert("Hello Ihjas");
+//Log to console (this is a single line command)
+/*  multi
+line command*/
+
+// now print something in console
+// In chrome developer tool
+
+console.log('Hello World');
+console.log(122);
+console.log(true);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
